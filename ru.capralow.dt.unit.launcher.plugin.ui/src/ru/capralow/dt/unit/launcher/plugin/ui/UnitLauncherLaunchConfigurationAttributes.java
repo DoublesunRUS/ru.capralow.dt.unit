0390b16ed5cdceb9e14a9d2d4ae257b31126b769
Капралов Александр <capralow@gmail.com>
2019-04-26 18:10:47 +0300

    Заготовка для Конфигурации запуска

---
+++ ru.capralow.dt.unit.launcher.plugin.ui/src/ru/capralow/dt/unit/launcher/plugin/ui/UnitLauncherLaunchConfigurationAttributes.java
@@ -0,0 +1,5 @@
+package ru.capralow.dt.unit.launcher.plugin.ui;
+
+public class UnitLauncherLaunchConfigurationAttributes {
+	public static final String CONSOLE_TEXT = "ru.capralow.dt.unit.launcher.plugin.ui.console.text";
+}
---
+++ ru.capralow.dt.unit.launcher.plugin.ui/src/ru/capralow/dt/unit/launcher/plugin/ui/UnitLauncherLaunchConfigurationTabGroup.java
@@ -0,0 +1,15 @@
+package ru.capralow.dt.unit.launcher.plugin.ui;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class UnitLauncherLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		setTabs(new ILaunchConfigurationTab[] { new UnitLauncherLaunchConfigurationTab(), new CommonTab() });
+	}
+
+}
