e3e7f589b3c0c8de7bec580537358ba64c60ca36
Капралов Александр <capralow@gmail.com>
2019-06-27 15:51:47 +0300

    Доработан тест получения списка модулей и тегов

--- ru.capralow.dt.unit.launcher.plugin.core.tests/src/ru/capralow/dt/unit/launcher/plugin/core/frameworks/tests/FrameworkUtilsTest.java
+++ ru.capralow.dt.unit.launcher.plugin.core.tests/src/ru/capralow/dt/unit/launcher/plugin/core/frameworks/tests/FrameworkUtilsTest.java
@@ -5,11 +5,15 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
 
 import ru.capralow.dt.unit.launcher.plugin.core.frameworks.FrameworkUtils;
 
 public class FrameworkUtilsTest {
+	private static final String PLUGIN_ID = "ru.capralow.dt.unit.launcher.plugin.core.tests"; //$NON-NLS-1$
 
 	@Test
 	public void testGetModulesForProjectEmpty() {
@@ -19,10 +23,30 @@ public class FrameworkUtilsTest {
 	}
 
 	@Test
+	public void testGetModulesForProjectNoFeatures() {
+		Bundle bundle = Platform.getBundle(PLUGIN_ID);
+		IPath resourcePath = Platform.getStateLocation(bundle);
+
+		List<String> modules = FrameworkUtils.getTestModules(resourcePath);
+
+		assertEquals("Список модулей: без фич", new ArrayList<String>(), modules); //$NON-NLS-1$
+	}
+
+	@Test
 	public void testGetTagsForProjectEmpty() {
 		List<String> tags = FrameworkUtils.getTestTags(null);
 
 		assertEquals("Список тегов: пустой", new ArrayList<String>(), tags); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testGetTagsForProjectNoFeatures() {
+		Bundle bundle = Platform.getBundle(PLUGIN_ID);
+		IPath resourcePath = Platform.getStateLocation(bundle);
+
+		List<String> tags = FrameworkUtils.getTestTags(resourcePath);
+
+		assertEquals("Список тегов: без фич", new ArrayList<String>(), tags); //$NON-NLS-1$
 	}
 
 }
